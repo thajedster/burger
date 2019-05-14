a73d0a68066539fa2348591eb54140661631f057
thajedster <jedlawrencecheng@gmail.com>
2019-05-13 22:28:24 -0400

    initialized files

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+drop database if EXISTS burgers_db;
+create database burgers_db;
+
+use DATABASE burgers_db;
+
+create table burgers
+(
+    id INT NOT NULL
+    auto_increment,
+    burger_name VARCHAR
+    (50),
+    devoured boolean
+);
+
