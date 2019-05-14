e97125d233188dffae3986cc4f7e77920d5d3a80
thajedster <jedlawrencecheng@gmail.com>
2019-05-14 03:23:04 -0400

    initialized models views and controllers files

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require('./controllers/burger_controller');
+app.use('/', routes);
+
+app.listen(PORT, function () {
+    console.log(`listening on PORT: ${PORT}`);
+});
+
