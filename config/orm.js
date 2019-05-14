b61fa627cfa69b3ebf7f47857f95265e2a8afc2d
thajedster <jedlawrencecheng@gmail.com>
2019-05-13 22:51:00 -0400

    created connection and started orm

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+var connection = require("./connection");
+
+var orm = {
+    selectAll: function () {
+
+    },
+    insertOne: function () {
+
+    },
+    updateOne: function () {
+
+    }
+}
+module.exports = orm;
